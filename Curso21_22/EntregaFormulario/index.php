67e13d5f8b98f63ac53056580412acd2b2d6ccbb
moreno27 <alejanmm2000@gmail.com>
2021-09-27 14:17:07 +0200

    correccion de la operacion OR (debe ser con "||")

--- Curso21_22/EntregaFormulario/index.php
+++ Curso21_22/EntregaFormulario/index.php
@@ -4,7 +4,7 @@
 if (isset($_POST["enviar"])) {
 
     //miramos los errores
-    $errores = (($_POST["name"] == "") | (!isset($_POST["sex"])));
+    $errores = (($_POST["name"] == "") || (!isset($_POST["sex"])));
 }
 
 //si se pulsa y no hay errores, mostramos el resultado de lo recibido con sus valores correspondientes
