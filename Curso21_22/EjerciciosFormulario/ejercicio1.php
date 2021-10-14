4dd21f34ec97e6ad27e618cfccf293dce695be11
moreno27 <alejanmm2000@gmail.com>
2021-10-14 11:02:50 +0200

    ejercicios formulario (inicio)

---
+++ Curso21_22/EjerciciosFormulario/ejercicio1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejrcicio1</title>
+</head>
+<body>
+    <form action="ejercicio1.php" method="get">
+        <label for="nombre">Nombre:</label>
+        <input type="text" name="name" id="nombre">
+
+        <br><br>
+        <label for="ape">Apellidos</label>
+        <input type="text" name="surname" id="ape">
+
+        <input type="submit" value="Enviar datos" name="enviar">
+    </form>    
+</body>
+</html>
