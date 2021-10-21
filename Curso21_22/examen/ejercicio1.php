62badd71af988312d34c2a71bd8e23cb9302dbf2
moreno27 <alejanmm2000@gmail.com>
2021-10-21 13:11:08 +0200

    comienzo del examen de php

---
+++ Curso21_22/examen/ejercicio1.php
@@ -0,0 +1,80 @@
+<?php
+if(isset($_POST["comprobar"])){
+	$errorTexto = $_POST["texto"] == "";
+	$errorSubcadena = $_POST["subcadena"] == "" || strlen($_POST["subcadena"])>strlen($_POST["texto"]);
+	$errores = $errorTexto || $errorSubcadena;
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ejercicio 1</title>
+	<meta charset="utf-8" />
+	<style>
+		.error{color: red;}
+	</style>
+</head>
+
+<body>
+	<h1>Ejercicio 1</h1>
+	<form action="ejercicio1.php" method="post">
+		<p>
+			<label for="palabra">Introduce palabra:</label>
+			<input type="text" name="texto" id="palabra" value="<?php if(isset($_POST["texto"])) echo $_POST["texto"];?>"/>
+			<?php
+				if(isset($_POST["comprobar"]) && $errorTexto){
+					echo "<span class='error'>*Campo obligatorio*</span>";
+				}
+			?>
+		</p>
+		<p>
+			<label for="subpalabra">Introduce letras:</label>
+			<input type="text" name="subcadena" id="subpalabra"value="<?php if(isset($_POST["subcadena"])) echo $_POST["subcadena"];?>"/>
+			<?php
+				if(isset($_POST["comprobar"]) && $errorSubcadena){
+					if($_POST["subcadena"] == "")
+						echo "<span class='error'>*Campo obligatorio*</span>";
+					else
+						echo "<span class='error'>*Imposible que este contenida en el texto anterior(este texto es mayor que el anterior)*</span>";
+				}
+			?>
+		</p>
+		<p>
+			<button type="submit" name="comprobar">Comprobar</button>
+		</p>
+	</form>
+	
+	<?php
+		if(isset($_POST["comprobar"]) && !$errores){
+		
+			for($i=0; $i <= strlen($_POST["texto"]) - strlen($_POST["subcadena"]); $i++){
+			
+				$igual = false;//lo inicializo a false
+				
+
+								
+				if($_POST["texto"][$i]==$_POST["subcadena"][0]){//si ambas son iguales, es candidata
+					$igual = true;
+				
+					for($j=0; $j<strlen($_POST["subcadena"]);$j++){//recorremos la longitud de la palabra
+						//si son iguales no pasara nada
+						if($_POST["texto"][$i+$j] != $_POST["subcadena"][$j]){
+							//si salta que son diferentes las letrtas comprobadas, salta "false" y termina el bucle
+							$igual=false;
+							break;
+						}
+					}
+				}
+				//cuando termian el bucle, si todo a sidop igual sera true, por lo tanto terminara el for principal
+				if($igual) break;
+				
+				//sino vuelta a empezar hasta que termine el texto
+			}
+			
+			if($igual) echo "Es cadena";
+			else echo "No es subcadena";
+			
+		}
+	?>
+</body>
+</html>
